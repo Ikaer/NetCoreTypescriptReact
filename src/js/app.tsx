3cff29d1edeae1fcf31d0087a24b06769f5c594a
Ikaer <ichaer@hotmail.com>
2018-08-27 06:04:10 +0200

    Push files

---
+++ src/js/app.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Component1 } from './component/component1';
+
+
+
+interface AppState { } 
+interface AppProps {}
+
+class App extends React.Component<AppProps, AppState>{
+    render() {
+        return (
+            <Component1 />
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
